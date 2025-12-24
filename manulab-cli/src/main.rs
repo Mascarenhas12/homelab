31703fe6cc45273b4b273f95241dca983fea267e
Mascarenhas12 <manuel.duarte.mascarenhas@gmail.com>
2025-12-24 16:38:20 +0100

    Pin versions for client and server; Explore logging and error handling

--- manulab-cli/src/main.rs
+++ manulab-cli/src/main.rs
@@ -1,3 +1,5 @@
+//use clap::Parser;
+use log::{info, warn};
 use manulab::resource_provider_client::ResourceProviderClient;
 
 pub mod manulab {
@@ -6,8 +8,15 @@ pub mod manulab {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = ResourceProviderClient::connect("http://[::1]:50051").await?;
-    println!("Conencted to Server!");
+    env_logger::init();
+    let mut client = match ResourceProviderClient::connect("http://[::1]:50051").await {
+        Ok(client) => client,
+        Err(error) => {
+          warn!("Problem connecting to server.Make sure server is up and running.");
+          panic!("Closing client: {error:?}");
+        }
+    };
+    info!("Connected to server!");
 
     let request = tonic::Request::new(());
     let response = client.get_plugin_info(request).await?;
@@ -16,3 +25,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+/*#[derive(Parser)]
+struct Cli {
+    command: Command;
+    args: Args;
+}
+
+fn main() {
+  let 
+}*/
