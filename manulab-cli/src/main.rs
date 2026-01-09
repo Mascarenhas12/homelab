a1661ced321f2bc52b34544b27027951341515f5
Mascarenhas12 <manuel.duarte.mascarenhas@gmail.com>
2026-01-09 22:37:20 +0100

    Prototype Cli only working to match first commands. Need to implement proper subcommands to call shipyard manifest --name manulab to call get-plugin-info demo

--- manulab-cli/src/main.rs
+++ manulab-cli/src/main.rs
@@ -1,18 +1,18 @@
-//use clap::Parser;
+use clap::{Parser, Subcommand};
 use log::{info, warn};
 use manulab::resource_provider_client::ResourceProviderClient;
+
 
 pub mod manulab {
     tonic::include_proto!("manulab");
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+async fn get_plugin_info() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = match ResourceProviderClient::connect("http://[::1]:50051").await {
         Ok(client) => client,
         Err(error) => {
-          warn!("Problem connecting to server.Make sure server is up and running.");
+          warn!("Problem connecting to server. Make sure server is up and running.");
           panic!("Closing client: {error:?}");
         }
     };
@@ -21,17 +21,75 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let request = tonic::Request::new(());
     let response = client.get_plugin_info(request).await?;
 
-    println!("RESPONSE={response:?}");
+    info!("RESPONSE={response:?}");
 
     Ok(())
 }
 
-/*#[derive(Parser)]
-struct Cli {
-    command: Command;
-    args: Args;
+/*
+#[derive(Debug, Clone)]
+enum ManifestFormat {
+    Text,
+    Json,
+    Yaml,
+    Table,
 }
 
-fn main() {
-  let 
-}*/
+#[derive(Debug, Subcommand, Clone)]
+enum ShipYardCommand {
+    Install{
+        #[arg(short, long)]
+        name: String,
+        #[arg(short, long, default_value_t = true)]
+        local: bool,
+        #[arg(short, long, default_value_t = "latest")]
+        version: String,
+    },
+    Manifest{
+        #[arg(short, long)]
+        name: String,
+        #[arg(short, long, default_value_t = ManifestFormat::Text)]
+        format: ManifestFormat,
+    },
+    Remove{
+        #[arg(short, long)]
+        name: String,
+    },
+}
+*/
+
+#[derive(Debug, Subcommand)]
+enum CaravelaCommand {
+    Shipyard,
+    Sail,
+    Wreck,
+    Spot,
+}
+
+#[derive(Parser, Debug)]
+#[command(name = "caravela", version, about, long_about = "Rust client to interact with Manulab GRPC server")]
+pub struct Cli {
+    #[clap(subcommand)]
+    command: CaravelaCommand,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+  env_logger::init();
+  let cli = Cli::parse();
+  match &cli.command {
+      CaravelaCommand::Shipyard => {
+          info!("Received command shipyard!");
+      },
+      CaravelaCommand::Sail => {
+          info!("Received command sail!");
+      },
+      CaravelaCommand::Wreck => {
+          info!("Received command wreck");
+      },
+      CaravelaCommand::Spot => {
+          info!("Received command spot");
+      },
+  }
+  Ok(())
+}
